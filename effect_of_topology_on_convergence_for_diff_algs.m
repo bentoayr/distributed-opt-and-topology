394a541d381b447b14e74333ce820d72b1963a74
bentoayr <35314774+bentoayr@users.noreply.github.com>
2019-03-16 16:23:25 -0400

    Add files via upload

---
+++ effect_of_topology_on_convergence_for_diff_algs.m
@@ -0,0 +1,26 @@
+%% testing different algorithms for distributed optimization
+
+n = 10;
+G = rand(n) > 0.5;
+
+[E1, E2] = find(G);
+
+numE = length(E1);
+
+X = rand(n,numE);
+Y = rand(n,numE);
+Theta = rand(n,numE);
+
+for e = 1:length(numE)
+   Theta = GradConjF(X(:,e),e);
+    
+end
+
+
+
+% this function computes the gradient of the 
+function [GRAD] = GradConjF(X, i)
+
+
+
+end
