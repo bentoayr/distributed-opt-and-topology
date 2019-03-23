3e2ced82c8da230d762dc78bb029e0c65a00fd01
jbento <jose.bento@bc.edu>
2019-03-23 15:09:55 -0400

    Playing with the way the parameters are defined for Alg 3 in second paper of BF. Doesn't seem that I can get fast convergence ever....

--- effect_of_topology_on_convergence_for_diff_algs.m
+++ effect_of_topology_on_convergence_for_diff_algs.m
@@ -21,16 +21,16 @@ numE = G.numedges;
 delta = 0.01;
 
 % Scaman et al. 2017, Algorithm 1 and Algorithm 2
-Y = 0*rand(dim, numE)*real(sqrtm(full(W)));
+Y = rand(dim, numE)*real(sqrtm(full(W)));
 X = Y;
 
 Theta = repmat(rand(dim,1),1,numE);
 Theta_old = Theta;
 
-num_iter = 500;
+num_iter = 1000;
 
 alpha = delta;
-beta  = 2;
+beta  = 2 + delta;
 kappa_l = beta / alpha;
 specW = sort(eig(W));
 eta = alpha/specW(end);
@@ -45,23 +45,25 @@ mu_2 = ((1 + c1^K)*sqrt(kappa_l) - 1 + c1^K) / ((1 + c1^K)*sqrt(kappa_l) + 1 - c
 
 
 R = 2; 
-L_is = 2*ones(numE , 1);
+L_is = (2+delta)*ones(numE , 1);
 L_l = norm(L_is,2)/sqrt(numE);
-eta_3  = (numE*R/L_l)*((1 - c1^K)/(1 + c1^K));
+eta_3  = ((1 - c1^K)/(1 + c1^K));%*(numE*R/L_l);
 sigma = (1 + c1^(2*K))/((1 - c1^K)^2);
-Theta_tilde = rand(dim,numE);
 sum_Theta = 0;
 
 
 M = num_iter;
 eps = 4*R*L_l/num_iter; % this is the target accuracy
+W_Acc = AccGoss(eye(numE), W, K,c2,c3);
 
-
-% tmp = randn(dim,1);
+% tmp = 0*randn(dim,1);
 % [X_out] = ProxF(tmp, 2, 0.4);
-% [X_out_2] = AppProxF(tmp, 2, 0.4,600);
-% norm(X_out-X_out_2)
+% [X_out_2] = AppProxF(tmp, 2, 0.4,1000,eta_3);
+% [norm(X_out-X_out_2), norm(X_out-tmp)]
+% X_out
 % return;
+
+
 
 
 Alg_name = 3;
@@ -76,10 +78,9 @@ if (Alg_name == 1)
         Y_old = Y;
         Y = X - eta*Theta*W;
         X = (1 + mu)*Y - mu*Y_old;
-        evol = [evol, log(norm(Y(:,1))) ];
+        evol = [evol, log(norm( Theta(:,:) - 1 ,1)) ];
         plot( [ 1 : t  ] , evol');
         drawnow;
-        %disp(t);
     end
 end
 
@@ -91,28 +92,40 @@ if (Alg_name == 2)
         Y_old = Y;
         Y = X - eta_2*AccGoss(X, W, K, c2, c3);
         X = (1 + mu_2)*Y - mu_2*Y_old;
-        evol = [evol, log(norm(Y(:,1))) ];
+        evol = [evol, log(norm( Theta(:,:) - 1 ,1)) ];
         plot([1:K:t*K],evol'); % each iteration corresponds to K gossip steps
         drawnow;
     end
 end
 
+
+sigma = 1/(eta_3*norm(full(W_Acc),2));
+%eta_3 = 1/(sigma*norm(full(W_Acc),2));
 if (Alg_name == 3)
+    
+    if (norm(full(W_Acc),2)*sigma*eta_3 > 1)
+        disp(['Convergene condition not met because ', num2str(norm(full(W),2)*sigma*eta_3), ' is bigger than 1']);
+        %return;
+    end
+       
+    
     for t = 1:num_iter
+
         Y = Y - sigma*AccGoss(2*Theta - Theta_old, W, K,c2,c3);
-        Theta_tilde(:,:) = Theta;
-        for e = 1:numE
-            
-            %for m = 1:M
-            %    Theta_tilde(:,e) = (m/(m+2))*Theta_tilde(:,e) - (2/(2+m))*((eta_3/numE)*GradF( Theta_tilde(:,e)  , e) - eta_3*Y(:,e) - Theta(:,e)  );
-            %end
-            Theta_tilde(:,e) = ProxF( eta_3*Y(:,e) + Theta(:,e) , e, 1/eta_3);
-            
-        end
+
         Theta_old = Theta;
+        Theta_tilde = Theta;
+        for e = 1:numE
+
+            Theta_tilde(:,e) = ProxF( eta_3*Y(:,e) + Theta(:,e) , e, 1/eta_3 );
+            %Theta_tilde(:,e) = AppProxF( eta_3*Y(:,e) + Theta(:,e) , e, 1/eta_3 , num_iter, eta_3);
+            %disp(   norm(   Theta_tilde(:,e) - tmp  )  );
+
+        end
         Theta = Theta_tilde;
-        sum_Theta = sum_Theta + sum(Theta,2)/(num_iter*numE);
-        evol = [evol, log(norm(sum_Theta)) ];
+        
+        sum_Theta = sum_Theta + sum(Theta,2)/numE;
+        evol = [evol, log(norm(sum_Theta/t - 1)) ];
         plot([1:K:t*K],evol'); % each iteration corresponds to K gossip steps
         drawnow;
     end
@@ -126,11 +139,11 @@ end
 function [GRAD] = GradConjF(X, i)
     global delta E1 E2
     d = delta;
+    X = X + d*ones(length(X),1);
     GRAD = (X)/delta;
     
     GRAD(E1(i)) = GRAD(E1(i)) + (1/(2*d + d*d))*(  -X(E1(i)) + X(E2(i))  );
     GRAD(E2(i)) = GRAD(E2(i)) + (1/(2*d + d*d))*(   X(E1(i)) - X(E2(i))  );
-
 end
 
 function [GRAD] = GradF(X, i)
@@ -141,29 +154,28 @@ function [GRAD] = GradF(X, i)
     GRAD(E1(i)) = GRAD(E1(i)) + (   X(E1(i)) - X(E2(i))  );
     GRAD(E2(i)) = GRAD(E2(i)) + (  -X(E1(i)) + X(E2(i))  );
 
+    GRAD = GRAD - d*ones(length(X),1);
 end
-
 
 function [X_out] = ProxF(X, i, rho)
     global E1 E2 numE delta
     
-    d = (rho*numE+delta);
+    X = X*numE*rho + delta;
+    
+    d = ( rho*numE + delta );
     
     X_out = (X)/d;
     
     X_out(E1(i)) = X_out(E1(i)) + (1/(2*d + d*d))*(  -X(E1(i)) + X(E2(i))  );
     X_out(E2(i)) = X_out(E2(i)) + (1/(2*d + d*d))*(   X(E1(i)) - X(E2(i))  );
-
-    X_out = X_out*numE*rho;
-    
 end
 
-function [X_out] = AppProxF(X, i, rho, M)
+function [X_out] = AppProxF(X, i, rho, M, alpha)
     global numE
 
     X_out = X;
     for m = 1:M
-        X_out = X_out - (2/(m+2))*( (1/numE)*GradF( X_out , i)  + rho*(X_out - X)     );
+        X_out = X_out - (2*alpha/(m+2))*( (1/numE)*GradF( X_out , i)  + rho*(X_out - X)     );
     end
     
 end
